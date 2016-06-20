4aa7c832f606ac318d3550ff177d689d08dbb4f6
motxx <motohiko.ave@gmail.com>
2016-06-20 15:54:30 +0900

    clang-format

--- repository/2601.cpp
+++ repository/2601.cpp
@@ -1,38 +1,43 @@
-#include <iostream>
 #include <algorithm>
-#include <cmath>
-#include <vector>
-#include <complex>
-#include <queue>
-#include <deque>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <iomanip>
-#include <assert.h>
 #include <array>
+#include <assert.h>
+#include <bitset>
+#include <cmath>
+#include <complex>
 #include <cstdio>
 #include <cstring>
-#include <random>
-#include <functional>
-#include <numeric>
-#include <bitset>
+#include <deque>
 #include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
 using namespace std;
 
-#define REP(i,a,b) for(int i=a;i<(int)b;i++)
-#define rep(i,n) REP(i,0,n)
+#define REP(i, a, b) for (int i = a; i < (int)b; i++)
+#define rep(i, n) REP(i, 0, n)
 #define all(c) (c).begin(), (c).end()
 #define zero(a) memset(a, 0, sizeof a)
 #define minus(a) memset(a, -1, sizeof a)
-#define watch(a) { cout << #a << " = " << a << endl; }
-template<class T1, class T2> inline bool minimize(T1 &a, T2 b) { return b < a && (a = b, 1); }
-template<class T1, class T2> inline bool maximize(T1 &a, T2 b) { return a < b && (a = b, 1); }
+#define watch(a)                                                               \
+  { cout << #a << " = " << a << endl; }
+template <class T1, class T2> inline bool minimize(T1 &a, T2 b) {
+  return b < a && (a = b, 1);
+}
+template <class T1, class T2> inline bool maximize(T1 &a, T2 b) {
+  return a < b && (a = b, 1);
+}
 
 typedef long long ll;
-int const inf = 1<<29;
+int const inf = 1 << 29;
 
 int N;
 int a[100010];
@@ -41,37 +46,35 @@ int dp[100010];
 int main() {
 
   cin >> N;
-  rep(i, N) {
-    cin >> a[i];
-  }
+  rep(i, N) { cin >> a[i]; }
 
   int x = 0;
   rep(i, N) {
-    x --;
-    if(a[i] == 0)
+    x--;
+    if (a[i] == 0)
       x = inf;
-    if(a[i] < 0)
+    if (a[i] < 0)
       minimize(x, -a[i]);
     maximize(dp[i], x);
   }
 
   x = 0;
-  for(int i=N-1; i>=0; i--) {
-    x --;
-    if(!a[i])
+  for (int i = N - 1; i >= 0; i--) {
+    x--;
+    if (!a[i])
       x = inf;
-    if(a[i] < 0)
+    if (a[i] < 0)
       minimize(x, -a[i]);
     maximize(dp[i], x);
   }
 
   int ans = 0;
   rep(i, N) {
-    if(a[i] > 0)
+    if (a[i] > 0)
       ans += min(dp[i], a[i]);
   }
 
   cout << ans << endl;
-  
+
   return 0;
 }
